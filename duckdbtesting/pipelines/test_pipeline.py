3d106b8277a542b182e300d7ec6b49ae2a8c0145
ananyaprabhakarm@gmail.com <ananyaprabhakarm@gmail.com>
2026-06-25 13:51:18 +0000

    Disable auto-layout in `test_pipeline` configuration

--- duckdbtesting/pipelines/test_pipeline.py
+++ duckdbtesting/pipelines/test_pipeline.py
@@ -1,6 +1,6 @@
 from prophecy_pipeline_sdk.graph import *
 from prophecy_pipeline_sdk.properties import *
-args = PipelineArgs(label = "test_pipeline", version = 1)
+args = PipelineArgs(label = "test_pipeline", version = 1, auto_layout = False)
 
 with Pipeline(args) as pipeline:
     test_pipeline__by_category_groupby_002 = Process(
