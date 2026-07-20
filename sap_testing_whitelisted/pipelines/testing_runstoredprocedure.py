aa272bddbe77bd60e7502e88b94d49c5ac5c742e
ananyaprabhakarm@gmail.com <ananyaprabhakarm@gmail.com>
2026-07-20 06:12:56 +0000

    ref

--- sap_testing_whitelisted/pipelines/testing_runstoredprocedure.py
+++ sap_testing_whitelisted/pipelines/testing_runstoredprocedure.py
@@ -3,6 +3,15 @@ from prophecy_pipeline_sdk.properties import *
 args = PipelineArgs(label = "testing_runstoredprocedure", version = 1, auto_layout = False)
 
 with Pipeline(args) as pipeline:
+    runstoredprocedure_1 = Process(
+        name = "RunStoredProcedure_1",
+        properties = RunStoredProc(
+          connectionType = "snowflake",
+          connector = "snowflake_default",
+          storedProcedure = "QA_DATABASE.QA_SCHEMA.test_add_numbers(5, 10)"
+        ),
+        input_ports = None
+    )
     call_stored_proc = Process(
         name = "call_stored_proc",
         properties = CallStoredProc(
@@ -12,12 +21,6 @@ with Pipeline(args) as pipeline:
            {"alias" : "NUM_B", "expression" : {"expression" : "NUM_B"}}],
           storedProcedureIdentifier = "QA_DATABASE.QA_SCHEMA.test_add_numbers"
         )
-    )
-    run_stored_proc = Process(
-        name = "run_stored_proc",
-        properties = RunStoredProc(connectionType = "mssql", storedProcedure = "QA_DATABASE.QA_SCHEMA.test_add_numbers(5, 10)"),
-        input_ports = None,
-        comment = "Executes a stored procedure to add numbers in the QA database for automated data processing."
     )
     test_numbers_seed = Process(
         name = "test_numbers_seed",
